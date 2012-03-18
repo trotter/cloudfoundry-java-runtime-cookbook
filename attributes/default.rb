2b45565b87b67c1baa294fd63913ea3f33577484
Trotter Cashion <cashion@gmail.com>
2012-03-18 10:51:39 -0700

    Initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,10 @@
+include_attribute "java"
+
+default[:cloudfoundry_dea][:runtimes][:java][:name]         = "java"
+default[:cloudfoundry_dea][:runtimes][:java][:cookbook]     = "cloudfoundry-java-runtime"
+default[:cloudfoundry_dea][:runtimes][:java][:executable]   = "java"
+default[:cloudfoundry_dea][:runtimes][:java][:version]      = "1.#{node.java.jdk_version}.0"
+default[:cloudfoundry_dea][:runtimes][:java][:version_flag] = "-version"
+
+default[:cloudfoundry_dea][:runtimes][:java][:debug_env][:run]     = ['JAVA_OPTS="$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=$VCAP_DEBUG_PORT,server=y,suspend=n']
+default[:cloudfoundry_dea][:runtimes][:java][:debug_env][:suspend] = ['JAVA_OPTS="$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=$VCAP_DEBUG_PORT,server=y,suspend=y"']
