5265307fbf51e61a35c7b19bb38e147af763058f
Trotter Cashion <cashion@gmail.com>
2012-03-19 16:34:47 -0700

    Define spring runtime

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,3 +8,5 @@ default[:cloudfoundry_dea][:runtimes][:java][:version_flag] = "-version"
 
 default[:cloudfoundry_dea][:runtimes][:java][:debug_env][:run]     = ['JAVA_OPTS="$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=$VCAP_DEBUG_PORT,server=y,suspend=n']
 default[:cloudfoundry_dea][:runtimes][:java][:debug_env][:suspend] = ['JAVA_OPTS="$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=$VCAP_DEBUG_PORT,server=y,suspend=y"']
+
+default[:cloudfoundry_cloud_controller][:server][:frameworks][:spring][:cookbook] = "cloudfoundry-java-runtime::spring"
